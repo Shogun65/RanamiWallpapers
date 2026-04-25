baa36f1659e6a87ff658ef6b7948292af9eedf9b
Chahat <arceusyt20@gmail.com>
2026-04-25 13:12:52 +0530

    Add file picker to pick file (ofc) and save that file path to txt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "Engine.h"
 #include <cstdlib>
 #include <string>
+#include <shobjidl.h>
 #include <fstream>
+
+#pragma comment(lib, "Ole32.lib")
 
 void SavePath(const std::string &filepath)
 {
@@ -24,10 +27,79 @@ std::string LoadPath()
 
 	std::getline(wallpaper_file_txt, file_path);
 
-	printf("file_path: %ws", file_path);
+	printf("file_path: %s\n", file_path.c_str());
 
 	return file_path;
 }
+
+std::string PickVideoFile()
+{
+    HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+    if (FAILED(hr))
+        return "";
+
+    IFileOpenDialog* pFileOpen = nullptr;
+
+    hr = CoCreateInstance(
+        CLSID_FileOpenDialog,
+        nullptr,
+        CLSCTX_ALL,
+        IID_IFileOpenDialog,
+        (void**)&pFileOpen
+    );
+
+    if (FAILED(hr))
+    {
+        CoUninitialize();
+        return "";
+    }
+
+    COMDLG_FILTERSPEC fileTypes[] =
+    {
+        { L"Video Files", L"*.mp4;*.mkv;*.avi;*.mov;*.wmv" },
+        { L"All Files", L"*.*" }
+    };
+
+    pFileOpen->SetFileTypes(2, fileTypes);
+    pFileOpen->SetTitle(L"Choose a wallpaper video");
+
+    hr = pFileOpen->Show(nullptr);
+
+    if (SUCCEEDED(hr))
+    {
+        IShellItem* pItem = nullptr;
+        hr = pFileOpen->GetResult(&pItem);
+
+        if (SUCCEEDED(hr))
+        {
+            PWSTR widePath = nullptr;
+            hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &widePath);
+
+            if (SUCCEEDED(hr))
+            {
+                char pathBuffer[512];
+                size_t converted = 0;
+                wcstombs_s(&converted, pathBuffer, sizeof(pathBuffer), widePath, _TRUNCATE);
+
+                std::string result = pathBuffer;
+
+                CoTaskMemFree(widePath);
+                pItem->Release();
+                pFileOpen->Release();
+                CoUninitialize();
+
+                return result;
+            }
+
+            pItem->Release();
+        }
+    }
+
+    pFileOpen->Release();
+    CoUninitialize();
+    return "";
+}
+
 
 int WINAPI wWinMain(
 	HINSTANCE hInstance, HINSTANCE ignore, LPWSTR pCmdLine, int nCmdShow)
@@ -35,33 +107,30 @@ int WINAPI wWinMain(
 	// ignore this
 	SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 	
-	const char* fileparth = "C:/Users/gmy87/Downloads/female-endministrator.3840x2160.mp4";
 	int buffersize = 3;
 
 	Engine engine;
 
-	wchar_t* fileflage = wcsstr(pCmdLine, L"-f-");
+	std::string video_path = LoadPath();
 
-	printf("fileFlage: %ws\n", fileflage);
-
-	if (fileflage)
+	if(video_path.empty())
 	{
-		// one extry for space -f- c the space between f-_c <- this if you can understand
-		// what iam saying
-		fileflage += 4;
-		 
-		static char filebuffer[512];
-
-		size_t converted = 0;
-
-		wcstombs_s(&converted, filebuffer, sizeof(filebuffer), fileflage, _TRUNCATE);
-
-		fileparth = filebuffer;
+		std::string picked_file_path = PickVideoFile();
+		SavePath(picked_file_path);
 	}
 
+	video_path = LoadPath();
 
-	engine.MakeWindowRunwhitWorkerWandRunDXandswapchinWhitFFmpeg(hInstance,
-		fileparth, buffersize);
+	if(video_path.empty())
+	{
+		MessageBox(nullptr, L"Video Path not exist!", L"Error", MB_ICONERROR);
+		return EXIT_FAILURE;
+	}
+
+	engine.MakeWindowRunwhitWorkerWandRunDXandswapchinWhitFFmpeg(
+		hInstance,
+		video_path.c_str(), 
+		buffersize);
 	
 	return EXIT_SUCCESS;
 }
