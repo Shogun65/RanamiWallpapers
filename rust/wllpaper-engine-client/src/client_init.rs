bfb35f191bf6f3c726ac07ea6f0dd1ba1698b766
Chahat <arceusyt20@gmail.com>
2026-06-17 08:47:41 +0530

    add files name to check before run

--- rust/wllpaper-engine-client/src/client_init.rs
+++ rust/wllpaper-engine-client/src/client_init.rs
@@ -1,5 +1,34 @@
+use std::path::Path;
 
 pub(crate) mod client_init
 {
     
+    fn client_init()
+    {
+        todo!("still in work");
+    }
+
+
+
+
+
+
+
+
+    fn check_files(path: &str)
+    {
+
+    }
+
+
+
+}
+
+mod files
+{
+    static FILES_CHECK_LIST: [&str; 9] = ["swscale-8.dll", "swresample-5.dll", 
+                                          "postproc-58.dll", "avutil-59.dll",
+                                          "avformat-61.dll", "avfilter-10.dll",
+                                          "avdevice-61.dll", "avcodec-61.dll",
+                                          "AliveWallpaperEngine.exe"];
 }
