98da8dd2ba17a02d966913e8ba5e8c0257fe5bf0
Chahat <arceusyt20@gmail.com>
2026-06-17 09:41:50 +0530

    idk i do bunch of stuff in client_init

--- rust/wllpaper-engine-client/src/client_init.rs
+++ rust/wllpaper-engine-client/src/client_init.rs
@@ -1,23 +1,26 @@
-use std::path::Path;
-
 pub(crate) mod client_init
 {
+    use std::path::Path;
+
     
-    fn client_init()
+    pub fn client_init() -> Result<(), String>
     {
         todo!("still in work");
     }
 
 
-
-
-
-
-
-
-    fn check_files(path: &str)
+    fn check_files()
     {
+        use super::log_err::log;
+        use super::files::FILES_CHECK_LIST;
 
+        for file in FILES_CHECK_LIST{
+            if !Path::new(file).exists()
+            {
+                log(&format!("MISSING FILE: {}", file));
+            }
+            println!("File found: {}", file);
+        }
     }
 
 
@@ -26,9 +29,39 @@ pub(crate) mod client_init
 
 mod files
 {
-    static FILES_CHECK_LIST: [&str; 9] = ["swscale-8.dll", "swresample-5.dll", 
-                                          "postproc-58.dll", "avutil-59.dll",
-                                          "avformat-61.dll", "avfilter-10.dll",
-                                          "avdevice-61.dll", "avcodec-61.dll",
-                                          "AliveWallpaperEngine.exe"];
+    pub(super) const FILES_CHECK_LIST: [&str; 9] = ["swscale-8.dll", "swresample-5.dll", 
+                                                    "postproc-58.dll", "avutil-59.dll",
+                                                    "avformat-61.dll", "avfilter-10.dll",
+                                                    "avdevice-61.dll", "avcodec-61.dll",
+                                                    "AliveWallpaperEngine.exe"];
+}
+
+pub(crate) mod log_err
+{
+    use std::fs::OpenOptions;
+    use std::io::Write;
+
+
+    pub fn log(message: &str)
+    {
+        let debug_file = OpenOptions::new()
+                                                .create(true)
+                                                .append(true)
+                                                .open("debug.txt");
+
+        let result = match debug_file {
+
+            Ok(mut debug) => writeln!(debug, "[ERROR] {}", message),
+            Err(err) => panic!("Error happand in engine also cant right log!, ERROR {}", err),
+
+        };
+
+        match result {
+
+            Ok(_) => {},
+            Err(err) => panic!("Cant write the ERROR: {}", err),
+
+        };
+
+    }
 }
