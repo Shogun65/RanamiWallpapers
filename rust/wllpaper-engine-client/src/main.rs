df2996b112c4022ddd81924b91f327b9c3cedc47
Chahat <arceusyt20@gmail.com>
2026-06-17 08:36:04 +0530

    started working on client rs

--- rust/wllpaper-engine-client/src/main.rs
+++ rust/wllpaper-engine-client/src/main.rs
@@ -1,5 +1,25 @@
+mod client_init;
+
+use std::{process::{Child, Command}};
+
 fn main() {
-    todo!("still in work!")
+    
+    let engine_process = match run_wallpaper_engine() {
+        Ok(process) => process,
+        Err(err) => {eprintln!("Error: {}", err); return;},
+    };
+
+    println!("engine_process: {}", engine_process.id());
+
 }
 
 
+fn run_wallpaper_engine() -> Result<Child, std::io::Error>
+{
+    let process = Command::new("AliveWallpaperEngine.exe")
+    .arg(r"C:\Users\gmy87\Downloads\ayanami-rei-beneath-blue-light.3840x2160.mp4")
+    .arg("3")
+    .spawn()?;
+
+    return Ok(process);
+}
