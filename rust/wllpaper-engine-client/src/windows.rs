f92b1129ffdba59eff0e0f032f9958e70b5685de
Chahat <arceusyt20@gmail.com>
2026-06-18 10:27:03 +0530

    create the main window and message loop

---
+++ rust/wllpaper-engine-client/src/windows.rs
@@ -0,0 +1,64 @@
+pub(crate) mod windows{
+    use std::thread;
+    use windows::Win32::Foundation::*; // idc man i want to code not to do this all day
+    use windows::Win32::UI::WindowsAndMessaging::*;
+    use windows::core::w;
+
+
+    fn run_main_window()
+    {
+
+        thread::spawn(||{
+            unsafe{
+                let class_name = w!("HiddenWindow");
+            
+                let wc = WNDCLASSW{
+                    lpfnWndProc : Some(window_proc),
+                    lpszClassName : class_name,
+                    ..Default::default()
+                };
+
+                RegisterClassW(&wc);
+
+                let hwnd = CreateWindowExW(
+                                Default::default(), 
+                                    class_name, 
+                                    w!(""), 
+                                    WS_POPUP,
+                                     0, 0, 0, 0, None, None, None, None);
+
+    
+                let mut msg = MSG::default();
+
+                while GetMessageW(&mut msg, None, 0, 0).into()
+                {
+                    let _ = TranslateMessage(&mut msg);
+                    DispatchMessageW(&mut msg);
+                }
+            }
+        });
+    }
+
+
+    extern "system" fn window_proc(
+    hwnd: HWND,
+    msg: u32,
+    wparam: WPARAM,
+    lparam: LPARAM,) -> LRESULT 
+    {
+        unsafe {
+
+            match msg {
+                
+                WM_DESTROY =>{
+                    PostQuitMessage(0);
+                    return LRESULT(0);
+                }
+
+                _ => return DefWindowProcW(hwnd, msg, wparam, lparam),
+            }
+            
+        }
+    }
+
+}
