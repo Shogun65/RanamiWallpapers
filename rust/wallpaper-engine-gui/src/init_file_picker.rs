e47221a2525a411fac7556e521c7b4756caf7a22
Chahat <arceusyt20@gmail.com>
2026-06-30 12:32:59 +0530

    iam idiot

---
+++ rust/wallpaper-engine-gui/src/init_file_picker.rs
@@ -0,0 +1,31 @@
+pub(crate) mod init{
+
+    use std::path::PathBuf;
+    use rfd::{AsyncFileDialog, FileDialog, FileHandle};
+
+    pub(crate) fn init_s_file_picker() -> Option<PathBuf> {
+
+
+        let file = FileDialog::new()
+                                .add_filter("video", &["mp4", "mkv", "avi", "mov","wmv"])
+                                .set_directory("/")
+                                .set_title("Alive-Wallpaper-Engine")
+                                .pick_file();
+    
+        return file;
+    
+    }
+
+    pub async fn init_a_file_picker() -> Option<FileHandle>{
+
+        let file_handle = AsyncFileDialog::new()
+                                .add_filter("video", &["mp4", "mkv", "avi", "mov","wmv"])
+                                .set_directory("/")
+                                .set_title("Alive-Wallpaper-Engine")
+                                .pick_file().await;
+        
+        return file_handle;
+    }
+
+
+}
--- rust/wallpaper-engine-gui/src/main.rs
+++ rust/wallpaper-engine-gui/src/main.rs
@@ -1,7 +1,7 @@
-mod init_s_file_picker;
+mod init_file_picker;
 mod file_saver;
 
-use init_s_file_picker::init::init_s_file_picker;
+use init_file_picker::init::init_s_file_picker;
 use file_saver::file_saver::{save_file_1, read_file_1};
 use shared::log_err::err_log;
 
