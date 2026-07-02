15870a5e99bf19d9b7caefe8d33323ab67e25581
Chahat <arceusyt20@gmail.com>
2026-07-02 09:21:54 +0530

    GUI UPDATE: work on GUI agian and now it clean and it still dont have anythink except colors but iam working on it

--- rust/wallpaper-engine-gui/build.rs
+++ rust/wallpaper-engine-gui/build.rs
@@ -1,9 +1,15 @@
 use shared::log_err::err_log;
+use slint_build::CompileError;
 
 fn main(){
 
     let compiler_result = slint_build::compile("ui/main-window.slint");
+    check_compiler_result(compiler_result);
     
+}
+
+fn check_compiler_result(compiler_result: Result<(), CompileError>)
+{
     match compiler_result {
         Ok(_) => {},
         Err(err) => {err_log(&format!("Error on slint compiler: {}", err)); return;},
--- rust/wallpaper-engine-gui/src/main.rs
+++ rust/wallpaper-engine-gui/src/main.rs
@@ -1,9 +1,9 @@
 slint::include_modules!();
 
-mod init_file_picker;
-mod file_saver;
+// mod init_file_picker;
+// mod file_saver;
 
-// use init_file_picker::init::init_s_file_picker;
+// use init_file_picker::init::init_a_file_picker;
 // use file_saver::file_saver::{save_file_1, read_file_1};
 // use shared::log_err::err_log;
 
@@ -12,7 +12,6 @@ mod file_saver;
 fn main() -> Result<(), slint::PlatformError> {
 
     let ui = AppWindow::new()?;
-
 
     return ui.run();
 
