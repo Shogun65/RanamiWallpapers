3d47bb1b9f4deb2695af034c2b07bf5fdc949966
Chahat <arceusyt20@gmail.com>
2026-07-07 15:34:46 +0530

    run now can init namepipe

--- rust/wallpaper-engine-client/src/client_loop.rs
+++ rust/wallpaper-engine-client/src/client_loop.rs
@@ -1,10 +1,34 @@
 pub mod init_loop{
 
-    pub fn run(
+    use crate::namepipe::init_namepipe;
+    use tokio::runtime::Runtime;
+    use core::time;
+    use std::{sync::{Arc, Mutex}, thread};
+    use shared::namepipe::NamePipeCommands;
 
-    )
+    pub fn run(
+        client_hwnd : usize,
+        namepipecommands : Arc<Mutex<NamePipeCommands>>
+    ) -> Result<(), std::io::Error>
     {
-        todo!("still in work");
+        println!();
+        let runtime = Runtime::new()?;
+        
+        let runtime_handle = runtime.handle(); 
+
+        let _handle_pipe = init_namepipe::run_namepipe_server(
+                                        namepipecommands.clone(), runtime_handle);
+
+        loop {
+            thread::sleep(time::Duration::from_secs(1));
+            println!("{:#?}", namepipecommands);
+        } 
+
+        return Ok(());
+    }
+
+    fn run_1(){
+        
     }
     
 }
--- rust/wallpaper-engine-client/src/main.rs
+++ rust/wallpaper-engine-client/src/main.rs
@@ -11,17 +11,15 @@ mod namepipe;
 
 use client_init::client_init::client_init;
 use client_init::error::ErrorClient;
-use engine::init_engine::run_wallpaper_engine;
 use client_init::log_err::err_log;
 use window::windows::init_window;
 use arg::{init, error};
-use shared::namepipe::{NamePipeCommands, PIPE_NAME};
-use core::time;
-use std::{sync::{Arc, Mutex}, thread};
-use namepipe::init_namepipe;
-use tokio::runtime::Runtime;
+use shared::namepipe::{NamePipeCommands};
+use std::sync::{Arc, Mutex};
+use client_loop::init_loop;
 
-fn main() {
+fn main() 
+{
 
     // that init fun for arg parse.. well sometime nameing get messy but anyways
     match init::init() {
@@ -39,45 +37,34 @@ fn main() {
                                                             wallpaper_changed : false
                                                         }));
 
-    // let client_result = client_init();
+    let client_result = client_init();
 
-    // match client_result {
-    //     Ok(_) => {},
-    //     Err(ErrorClient::MissingFile(file_name)) => {
-    //         err_log(&format!("MissingFile: {}", file_name)); return;},
-    // };
+    match client_result {
+        Ok(_) => {},
+        Err(ErrorClient::MissingFile(file_name)) => {
+            err_log(&format!("MissingFile: {}", file_name)); return;},
+    };
 
-    // let init_window_data = init_window();
+    let init_window_data = init_window();
 
-    // let window_handle = init_window_data.handle;
+    let _window_handle = init_window_data.handle;
 
-    // let client_hwnd = init_window_data.main_hwnd; // iknow main hwnd and client hand geting mess but they both same thing hehe
+    let client_hwnd = init_window_data.main_hwnd; // iknow main hwnd and client hand geting mess but they both same thing hehe
 
-    // let client_hwnd = match client_hwnd {
+    let client_hwnd = match client_hwnd {
 
-    //     Some(hwnd) => {
-    //         println!("client_hwnd: {}", hwnd); 
-    //         hwnd
-    //     },
+        Some(hwnd) => {
+            println!("client_hwnd: {}", hwnd); 
+            hwnd
+        },
 
-    //     None => {
-    //         err_log("main_hwnd is None!!!");
-    //         return;
-    //     },
-    // };
+        None => {
+            err_log("main_hwnd is None!!!");
+            return;
+        },
+    };
 
-
-    let runtime = Runtime::new().unwrap();
-
-    let runtime_handle = runtime.handle();
-
-    let _namepipe_handle = init_namepipe::run_namepipe_server(namepipecommands.clone(), runtime_handle);
-
-    loop {
-        thread::sleep(time::Duration::from_secs(1));
-        println!("{:#?}", namepipecommands);
-    }
-
+    let _ = init_loop::run(client_hwnd, namepipecommands);
 }
 
 
