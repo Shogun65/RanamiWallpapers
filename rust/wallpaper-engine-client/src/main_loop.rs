6baf2db8e779ea7e1812690234d0e8344db8c210
Chahat <arceusyt20@gmail.com>
2026-07-08 09:04:28 +0530

    add new files and also make new file in rust for loop
    lest loop was soo idiot that i make i get some ref from GPT and now i have some cleam pic on my mind how i am going to make now hopefully

---
+++ rust/wallpaper-engine-client/src/main_loop.rs
@@ -0,0 +1,24 @@
+use std::sync::{Arc, Mutex};
+
+use shared::namepipe::NamePipeCommands;
+
+pub fn main_loop(
+
+    namepipecommands : Arc<Mutex<NamePipeCommands>>,
+    client_hwnd : usize
+
+
+) -> Result<(), std::io::Error>
+{
+
+
+
+
+
+
+
+
+
+
+    return Ok(());
+}
