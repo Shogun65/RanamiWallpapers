c10907eab633cfc737088a5eceb0d5d3889deec7
Chahat <arceusyt20@gmail.com>
2026-07-10 09:31:49 +0530

    get ready

--- rust/ranami-wallpapers/src/init_tray.rs
+++ rust/ranami-wallpapers/src/init_tray.rs
@@ -1,6 +1,9 @@
 use std::process::{Child, Command};
 
 
-// pub fn run_tray() -> Result<Child, std::io::Error>{
-//     let result = Command::new("");
-// }
+pub fn run_tray(engine_hwnd : usize) -> Result<Child, std::io::Error>{
+    let result = Command::new("ranami-wallpapers-tray.exe")
+                        .arg(engine_hwnd.to_string())
+                        ;
+    todo!()
+}
